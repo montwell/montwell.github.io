ba54730583124f07df0a4f43abcf2ca5cafae3b4
Jonathan Montwell <montwell@gmail.com>
2020-08-01 16:38:53 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function loadMap() {
+async function loadMap() {
 	
 	var width = 1400;
 	var height = 700;
-	var townMap = d3.json("data/ct-towns.geojson");
+	var townMap = await d3.json("data/ct-towns.geojson");
 	var projection = d3.geoMercator().translate([width/2, height/2]).scale(2200).center([0,40]);
     var path = d3.geoPath().projection(projection);
 	
