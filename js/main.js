9ca09868af8f13c50c4c90d16e9f5ccfe9a608f1
Jonathan Montwell <montwell@gmail.com>
2020-08-01 16:34:00 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 function loadMap() {
 	
 	var width = 1400;
-	var height = 7000;
+	var height = 700;
 	var townMap = d3.json("data/ct-towns.geojson");
 	var projection = d3.geoMercator().translate([width/2, height/2]).scale(2200).center([0,40]);
     var path = d3.geoPath().projection(projection);
 	
-	svg = d3.select("#map")
+	var svg = d3.select("#map")
 	  .append("svg")
 	  .attr('width', width)
 	  .attr('height', height);
 	  
-	g = svg.append("g").attr("class", "g-town")
+	var g = svg.append("g").attr("class", "g-town")
 	
-	town = g.selectAll("path.town")
+	var town = g.selectAll("path.town")
 	  .data(townMap.features)
 	  .enter()
 	  .append("path")
