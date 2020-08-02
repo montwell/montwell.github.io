1418e4356a63686fa4746ba6ed764b1cac2cdbae
Jonathan Montwell <montwell@gmail.com>
2020-08-02 10:46:53 -0400

    update

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ async function init() {
 	
 	covidDataByTown = d3.nest()
 		.key(function(d) {return d["Town number"];})
-		.sortKeys(a,b => a > b)
+		.sortKeys((a,b) => a > b)
 		.key(function(d) {return d["Last update date"];})
 		.entries(csvCovidData);
 	
