a797487d242756b37b4522a997070aca7983aa94
Jonathan Montwell <montwell@gmail.com>
2020-08-02 16:01:24 -0400

    update

--- js/main.js
+++ js/main.js
@@ -87,12 +87,12 @@ function drawCasesGraph(townId){
 			.range([0, gWidth]);
 			
 		var y = d3.scaleLinear()
-			.domain(d3.max(townData.values, d => d.values[0]["Total deaths"]))
-			.range([gHeight, 0]);
+			.domain(d3.max(townData.values, d => d.values[0]["Total cases "]))
+			.range([0, gHeight]);
 			
 		
 		svg.append("g")
-			.attr("transform", "translate(0," + gHeight + ")")
+			.attr("transform", "translate(" + margin.left + "," + (gHeight + margin.top) + ")")
 			.call(d3.axisBottom(x).ticks(5));
 			
 		svg.append("g")
