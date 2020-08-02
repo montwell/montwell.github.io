c09a70c8e8e594491fb20ea06a9965d8bad9778e
Jonathan Montwell <montwell@gmail.com>
2020-08-02 14:10:16 -0400

    update

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ async function init() {
 	mapColorScale = d3.scaleLinear()
 		.domain([0,maxCases])
 		//.range(["#FFDDDD","FF0000"]);
-		.range(["grey","red"]);
+		.range(["whitesmoke","red"]);
 	
 	covidDataByTown = d3.nest()
 		.key(d => d["Town number"])
