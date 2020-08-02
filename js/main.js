3d4e1cab7df2c418e43cd176d3f1fa953495a5dd
Jonathan Montwell <montwell@gmail.com>
2020-08-02 05:11:49 -0400

    update

--- js/main.js
+++ js/main.js
@@ -1,8 +1,17 @@
-async function loadMap() {
+var geojson;
+var covidData;
+
+async function init() {
 	
+	geoJson = await d3.json("data/ct-towns.geojson");
+	// load Covid Data
+	
+	drawMap();
+}
+
+function drawMap() {
 	var width = 1000;
-	var height = 700;
-	var townMap = await d3.json("data/ct-towns.geojson");
+	var height = 700;	
 	var projection = d3.geoMercator().translate([width/2, height/2]).scale(25000).center([-72.6, 41.5]);
     var path = d3.geoPath().projection(projection);
 	
@@ -14,10 +23,10 @@ async function loadMap() {
 	var g = svg.append("g").attr("class", "g-town")
 	
 	var town = g.selectAll("path")
-	  .data(townMap.features)
+	  .data(geojson.features)
 	  .enter()
 	  .append("path")
 	  .attr("class", "town")
 	  .attr('d', path)
-	  .style('stroke', "white");	
+	  .style('stroke', "white");
 }
