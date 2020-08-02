27a0c57eb727fa5f4853ca45cb1e94f3b3256dc3
Jonathan Montwell <montwell@gmail.com>
2020-08-02 08:29:46 -0400

    update

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ async function init() {
 	console.log("version 0.02");
 	
 	geojson = await d3.json("data/ct-towns.geojson");
-	var csvCovidData = await d3.json("data/covid-by-town.csv");
+	var csvCovidData = await d3.csv("data/covid-by-town.csv");
 	
 	covidDataByTown = csvCovidData.nest()
 		.key(function(d) {return d["Town number"];})
