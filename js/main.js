aeddcb98310e93a077d12662aae0eb3b783ea2c7
Jonathan Montwell <montwell@gmail.com>
2020-08-03 09:14:14 -0400

    update

--- js/main.js
+++ js/main.js
@@ -108,7 +108,7 @@ function drawCasesGraph(townId){
 		
 		svg.append("g")
 			.attr("transform", "translate(" + margin.left + "," + (gHeight + margin.top) + ")")
-			.call(d3.axisBottom(x).ticks(6).tickFormat(d3.timeFormat("%B %e %Y")));
+			.call(d3.axisBottom(x).ticks(5));
 			
 		svg.append("g")
 			.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
