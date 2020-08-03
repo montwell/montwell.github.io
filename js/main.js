00671ea9fbc208dfd299f91a2eae0b8620964d8d
Jonathan Montwell <montwell@gmail.com>
2020-08-02 20:38:30 -0400

    update

--- js/main.js
+++ js/main.js
@@ -275,13 +275,13 @@ function onClickTown(path, townId) {
 		d3.select("#graphs").transition().duration(800).style("visibility", "hidden");
 		drawCasesGraph(townId);
 		drawDeathsGraph(townId);
-		d3.select("#graphs").transition().duration(1200)).style("visibility", "visible");
+		d3.select("#graphs").transition().duration(1200).style("visibility", "visible");
 		
 	} else {
 		console.log("Town unclicked");
 		previouslySelectedTownId = null;
 		previouslySelectedTown = null;
-		d3.select("#graphs").transition().duration(800)).style("visibility", "hidden");
+		d3.select("#graphs").transition().duration(800).style("visibility", "hidden");
 	}
 }
 
